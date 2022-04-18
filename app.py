c41bb50ad6f41baf632e213eb3b8c3696f147474
Brian Elizondo <brian_elizondo@msn.com>
2022-04-17 21:54:39 -0400

    Step Two: Displaying the Board => Generate a board on the backend using a function from the boggle.py file and sending that to Jinja template and display the board on the page. For use this board in other routes, place it in the session. Add a form that allows a user to submit a guess

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, session
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -9,4 +9,7 @@ boggle_game = Boggle()
 @app.route('/')
 def home_page():
     """Return a page that shows Boggle game board"""
-    return render_template("home.html")
+
+    session['boggle_board'] = boggle_game.make_board()
+    
+    return render_template("home.html", board=session['boggle_board'])
