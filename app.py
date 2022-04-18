631575818217890983e85634094fc854ff3959ee
Brian Elizondo <brian_elizondo@msn.com>
2022-04-17 21:11:39 -0400

    Step One: Planning and reading starter code

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import Flask, render_template, redirect
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abcd1234"
+
 boggle_game = Boggle()
+
+@app.route('/')
+def home_page():
+    """Return a page that shows Boggle game board"""
+    return render_template("home.html")
